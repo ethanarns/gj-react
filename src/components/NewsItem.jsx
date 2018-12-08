df276040022cf583316036cfd90eb3e386d57bce
Ethan Arns <narkro555@gmail.com>
2018-12-08 10:24:41 -0700

    Added properties to NewsItem

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -4,13 +4,13 @@ class NewsItem extends Component {
     render() {
         return (
             <div class="list-group-item">
-            <div class="d-flex w-100 justify-content-between">
-                <h6 class="m-0">Next meeting January 23</h6>
-                <small>12-4-2018</small>
-            </div>
-            <p class="m-0 text-muted">
-                2pm at Nob Hill Capitola Wine Bar
-            </p>
+                <div class="d-flex w-100 justify-content-between">
+                    <h6 class="m-0">{this.props.maintext}</h6>
+                    <small>{this.props.date}</small>
+                </div>
+                <p class="m-0 text-muted">
+                    {this.props.subtext}
+                </p>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import News from './components/News';
 import * as serviceWorker from './serviceWorker';
-import $ from 'jquery';
 
 // Renders "newsCol"
-ReactDOM.render(<News />, document.getElementById('newsRoot'));
-$('#newsCol').unwrap(); // Delete newsRoot and replace it
+ReactDOM.render(<News />, document.getElementById('newsCol'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
